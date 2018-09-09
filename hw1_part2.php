26ff0fd0efb14a1a6531775781cacc58e6bf2322
Kostya Morenov <kostya.morenov@gmail.com>
2018-09-10 02:42:36 +0300

    version 1.2.1

--- hw1_part2.php
+++ hw1_part2.php
@@ -7,47 +7,58 @@
  */
 class SecondTask
 {
-//Задачи по Массивам: 5,6,7,8) Написать свою реализацию следующих функций php: array_sum, in_array, array_diff , sort.
-    function arraySum($arr, $check)
+    private $arr;
+    public function __construct($arr)
     {
-        echo "<b>5) array_sum </b><br> Original Array: ";
-        var_dump( $arr);
-        /********** FUNCTION *************/
-        echo "<b>AFTER: array_sum function : </b>" . array_sum($arr);
-        /*********************************/
-
-        for ($i = 0; $i < count($arr); $i++) {
-            $arrSum += $arr[$i];
-        }
-        echo " -----<b> Manual Sum: $arrSum </b><br/>";
+        $this->arr = $arr;
     }
 
-    function inArray($check = 31, $arr)
+//Задачи по Массивам: 5,6,7,8) Написать свою реализацию следующих функций php: array_sum, in_array, array_diff , sort.
+    function arraySum()
+    {
+        echo "<b>5) array_sum </b><br> Original Array: ";
+        var_dump( $this->arr);
+        /********** FUNCTION *************/
+        echo "<b>AFTER: array_sum function : </b>" . array_sum($this->arr);
+        /*********************************/
+
+        $arrSum = 0;
+        for ($i = 0; $i < count($this->arr); $i++) {
+            $arrSum += $this->arr[$i];
+        }
+        return " -----<b> Manual Sum: $arrSum </b><br/>";
+    }
+
+    function inArray($check = 6)
     {
         /********** FUNCTION *************/
-        echo "<br/><b>6) </b> $check <b>in_array function: </b>" . boolval(in_array($check, $arr)) . "<br/>";
+        echo "<br/><b>6) </b> $check <b>in_array function: </b>" . boolval(in_array($check, $this->arr)) . "<br/>";
         /*********************************/
         $found = false;
-        foreach ($arr as $item) {
+        foreach ($this->arr as $item) {
             if ($item == $check) {
                 $found = true;
                 return "<b>$check In Array manual check:</b> $found <br/>";
             }
         }
+        return "<b>$check In Array manual check</b> NOT found <br/>";
     }
 
-    function arrayDiff($arr, $check)
+    function arrayDiff(array $check)
     {
         echo "<br/><b>7) array_diff</b><br/>";
-        echo "BEFORE: " . var_dump($arr);
+        echo 'BEFORE. Array to compare from.';
+        var_dump($this->arr);
+        echo 'Array to compare against.';
+        var_dump($check);
         /********** FUNCTION *************/
-        $diff[] = array_diff($arr, $check);
+        $diff[] = array_diff($this->arr, $check);
         echo '<br/><b>FUNCTION array_diff: </b>';
         var_dump($diff);
         /*********************************/
 
         $res = [];
-        foreach ($arr as $value1) {
+        foreach ($this->arr as $value1) {
             $found = false;
             foreach ($check as $value2) {
                 if ($value1 == $value2) {
@@ -61,10 +72,10 @@ class SecondTask
         return $res;
     }
 
-    function arraySort($arr)
+    function arraySort()
     {
         echo "<b>8) Bubble Sort.</b><br/>BEFORE sorting";
-        var_dump($arr);
+        var_dump($this->arr);
 
         /********** FUNCTION *************/
 //        echo '<br/><b>FUNCTION sort: </b>';
@@ -72,29 +83,28 @@ class SecondTask
 //        var_dump($arr);
         /*********************************/
 
-        $size = count($arr);
+        $size = count($this->arr);
         for ($i = 0; $i < $size; $i++) {
             for ($j = 0; $j < $size-1; $j++) {
-                if ($arr[$j] > $arr[$j+1]) {
-                    $temp = $arr[$j];
-                    $arr[$j] = $arr[$j+1];
-                    $arr[$j+1] = $temp;
+                if ($this->arr[$j] > $this->arr[$j+1]) {
+//                  $temp = $this->arr[$j]; $this->arr[$j] = $this->arr[$j+1]; $this->arr[$j+1] = $temp;
+                    list ($this->arr[$j + 1], $this->arr[$j]) = [$this->arr[$j], $this->arr[$j + 1]];
                 }
             }
         }
         echo 'AFTER MANUAL sorting';
-        return $arr;
+        return $this->arr;
     }
 
 }
-$fifth = new SecondTask();
-echo $fifth->arraySum([0, 11, 204, 31, 4, 5], [30, 4, 51, 6, 7]);
+$fifth = new SecondTask($arr = [0, 11, 204, 31, 4, 5]);
+echo $fifth->arraySum();
 
-$sixth = new SecondTask();
-echo $sixth->inArray(5, $arr = [1, 6, 5, 5, 31]);
+$sixth = new SecondTask($arr = [1, 6, 5, 5, 31]);
+echo $sixth->inArray();
 
-$seventh = new SecondTask();
-var_dump($seventh->arrayDiff([0, 11, 204, 31, 4, 5], [30, 4, 51, 6, 7, 31]));
+$seventh = new SecondTask($arr = [0, 11, 204, 31, 4, 5]);
+var_dump($seventh->arrayDiff([30, 4, 51, 6, 7, 31]));
 
-$eighth = new SecondTask();
-var_dump($eighth->arraySort($arr = [0, 11, 204, 31, 4, 5, 6, 5]));
+$eighth = new SecondTask($arr = [0, 11, 204, 31, 4, 5, 6, 5]);
+var_dump($eighth->arraySort());
